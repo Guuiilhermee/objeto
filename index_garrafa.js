8dd271221b89e70b200b2864ac60801d8a9734fe
Guilherme Ambrosio <guiilherme.ambrosio@gmail.com>
2026-02-18 16:45:41 -0300

    obj

--- index_garrafa.js
+++ index_garrafa.js
@@ -1,9 +1,20 @@
 const Garrafa = require(`./objeto`)
 
 let garrafa = new Garrafa('medio', 2, 2, 'azul', 'azul')
+let garrafa2 = new Garrafa('grande', 3, 3, 'vermelho', 'vermelho')
 
 let area = garrafa.calcularAreaBase()
 console.log(`A área da garrafa é ${area.toFixed(3)} m²`)
 
 let volume = garrafa.calcularVolume()
 console.log(`O volume da garrafa é ${volume.toFixed(3)}`)
+
+garrafa.imprimir(volume)
+
+let area2 = garrafa2.calcularAreaBase()
+console.log(`A área da garrafa2 é ${area2.toFixed(3)} m²`)
+
+let volume2 = garrafa2.calcularVolume()
+console.log(`O volume da garrafa2 é ${volume2.toFixed(3)}`)
+
+garrafa.imprimir(volume2)
